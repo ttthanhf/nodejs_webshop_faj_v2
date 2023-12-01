8704c8854e8ae1767181d8612c3ef0643e6f610b
ThanhF <trthanh179@gmail.com>
2023-12-01 11:31:46 +0700

    REMOVE - Remove node_modules and test folder

--- test/test.js
+++
@@ -1,3 +0,0 @@
-const jwt = require('../src/utils/jsonwebtoken');
-console.log(jwt.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpaCI6MCwidXNlcm5hbWUiOiJhZG1pbiIsInJvbGUiOiIwIiwiaWF0IjoxNjg4NDYwOTExfQ.DDr7X498w9KnVVX3RZ_dZNc8AODY6dlGFa_T5bGvLdo'))
-    
